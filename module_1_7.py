91fc9cbee643ada19b6219dadd48eef93320a129
Sveta-ai <stralev@mail.ru>
2024-09-16 23:21:57 +0300

    Светлана Стратилатова Дополнительное практическое задание по модулю "Базовые структуры данных"

---
+++ module_1_7.py
@@ -0,0 +1,38 @@
+grades=[[5,3,3,5,4],[2,2,2,3],[4,5,5,2],[4,4,3],[5,5,5,4,5]]
+student={"Johnny","Bilbo","Steve","Khendrik","Aaron"}
+student=list(student)
+print(student)
+sorted_list_student=sorted(student)
+print(sorted_list_student)
+inp_lst=[5,3,3,5,4]
+sum_lst=sum(inp_lst)
+print(sum_lst)
+lst_avg=sum_lst/len(inp_lst)
+print("Average value of the list:\n")
+print(lst_avg)
+inp_lst=[2,2,2,3]
+sum_lst=sum(inp_lst)
+print(sum_lst)
+lst_avg=sum_lst/len(inp_lst)
+print("Average value of the list:\n")
+print(lst_avg)
+inp_lst=[4,5,5,2]
+sum_lst=sum(inp_lst)
+print(sum_lst)
+lst_avg=sum_lst/len(inp_lst)
+print("Average value of the list:\n")
+print(lst_avg)
+inp_lst=[4,4,3]
+sum_lst=sum(inp_lst)
+print(sum_lst)
+lst_avg=sum_lst/len(inp_lst)
+print("Average value of the list:\n")
+print(lst_avg)
+inp_lst=[5,5,5,4,5]
+sum_lst=sum(inp_lst)
+print(sum_lst)
+lst_avg=sum_lst/len(inp_lst)
+print("Average value of the list:\n")
+print(lst_avg)
+grades_student={"Aaron":4.0,"Bilbo":2.25, "Johnny":4.0, "Khendrik":3.6666666666666665, "Steve":4.8}
+print(grades_student)
