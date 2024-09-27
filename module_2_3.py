797cc5450122de76bcfafc83bf919349f8d35a28
Sveta-ai <stralev@mail.ru>
2024-09-27 21:24:31 +0300

    Светлана Стратилатова "Стиль кода часть II.Цикл While"

---
+++ module_2_3.py
@@ -0,0 +1,21 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+
+index = -1
+
+while index < len(my_list):
+    index = index + 1
+    if my_list[index] < 0:
+        break
+    if my_list[index] == 0:
+        continue
+    print(my_list[index])
+
+
+print('---------')
+
+
+while index < len(my_list) and my_list[index] >= 0:
+    if my_list[index] != 0:
+        print(my_list[index])
+    index = index + 1
+
