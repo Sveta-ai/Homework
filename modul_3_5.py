636fd309745111bb51e7df75a0c8c9f42a74ac99
Sveta-ai <stralev@mail.ru>
2024-10-29 22:02:05 +0300

    Светлана Стратилатова "4 модуль Пространство имен"

--- modul_3_5.py
+++ modul_3_5.py
@@ -1,13 +1,11 @@
-def get_multiplied_digits(number):
-    str_number = str(number)
+def get_multiplied_digits (numbers):
+  str_number = str(numbers)
+  first = int(str_number[0])
+  if len(str_number) > 1:
+    return first * get_multiplied_digits(int(str_number[1:]))
+  else:
+    return first
+a = get_multiplied_digits(40203)
+print(a)
 
-    first = int(str_number[0])
 
-    if len(str_number) > 1:
-        return first * get_multiplied_digits(int(str_number[1:]))
-    else:
-        return first
-
-
-result = get_multiplied_digits(40203)
-print(result)
